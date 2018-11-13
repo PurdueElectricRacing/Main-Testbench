b6a48eeff446893eb61c077a3a91461562db8112
AdamBusch <adamjbusch561@gmail.com>
2018-11-13 01:22:26 -0500

    Add initial CANDapter implementation
    This copies the CAN functionality from the wireless-telemetry repo.

---
+++ CANAdapterDevices.py
@@ -0,0 +1,56 @@
+import serial
+
+
+class GenericCANAdapterDevice:
+    def __init__(self):
+        pass
+
+    def sendSerialMessage(self, message):
+        pass
+
+    def sendCANMessage(self, id, message):
+        pass
+
+    def readCANMessage(self):
+        pass
+
+
+class CANDapterDevice(GenericCANAdapterDevice):
+    def __init__(self,
+                 port='/dev/ttyUSB0',
+                 baudrate=2500000,
+                 timeout=None,
+                 debug=False):
+
+        super().__init__()
+        self.canDapterDevice = serial.Serial(port, baudrate, timeout=timeout)
+
+    def sendSerialMessage(self, message):
+        self.canDapterDevice.write(message + '\r')
+
+    def sendCANMessage(self, can_id, message):
+        data_length = len(str(message)) / 2
+
+        if(data_length % 2 == 1):
+            raise ValueError('Invalid data lenght provided to CAN message')
+
+        self.sendSerialMessage('T%(id)s%(length)s%(data)x\r' % {
+            'id': can_id,
+            'length': data_length,
+            'data': message
+        })
+
+    def readCANMessage(self):
+        message = self.canDapterDevice.read_until('\r').replace('\r', '')[1:]
+
+        m_id = message[0:3]
+        m_len = message[3:4]
+        m_message = message[4:-4]
+        m_time_stamp = str(datetime.datetime.now())
+
+        return {
+            'can_id': m_id,
+            'length': m_len,
+            'message': m_message,
+            'timestamp': m_time_stamp
+        }
---
+++ Main-Testbench.py
@@ -0,0 +1,16 @@
+from CANAdapterDevices import CANDapterDevice
+
+canDapter = CANDapterDevice()
+
+# Set bitrate to 500Kbit and open CANDapter
+canDapter.sendSerialMessage('S6')
+canDapter.sendSerialMessage('O')
+
+while True:
+    data = canDapter.readCANMessage()
+
+    print('-- ID:%(id)x \tMessage:%(message)s \tTimestamp: %(time)s' % {
+            'id': data['id'],
+            'message': data['message'],
+            'time': data['timestamp']
+    })
